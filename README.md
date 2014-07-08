2c705513d99d5eec98dfe923d897fbde1cea46de
clux <analsandblaster@gmail.com>
2014-07-08 23:45:00 +0100

    make it work

---
+++ README.md
@@ -0,0 +1,33 @@
+# Badgify
+[![Build Status](https://secure.travis-ci.org/clux/badgify.svg)](http://travis-ci.org/clux/badgify)
+[![Dependency Status](https://david-dm.org/clux/badgify.svg)](https://david-dm.org/clux/badgify)
+[![experimental](http://img.shields.io/badge/stability-experimental-DD5F0A.svg)]((http://nodejs.org/api/documentation.html#documentation_stability_index))
+
+## Usage
+Install and use in a package root
+
+```
+$ badgify
+```
+
+Output:
+
+```
+[![Build Status](https://secure.travis-ci.org/clux/dye.svg)](http://travis-ci.org/clux/dye)
+[![Dependency Status](https://david-dm.org/clux/dye.svg)](https://david-dm.org/clux/dye)
+[![Coverage Status](http://img.shields.io/coveralls/clux/dye.svg/badge.png)](https://coveralls.io/repos/clux/dye)
+[![unstable](http://img.shields.io/badge/stability-unstable-E5AE13.svg)]((http://nodejs.org/api/documentation.html#documentation_stability_index))
+```
+
+## How
+- If your repo has the `.travis.yml` file it will generate the svg badge that links to the travis report.
+- It will always generate the david-dm link for your dependencies
+- If your `package.json` has the `coveralls` script it will generate the svg badge that links to your coveralls.io report
+- Stability badge will be provided based on `stability` key in package.json or fallback to experimental
+
+
+## Future
+Maybe auto-modify the `README.md`..
+
+## License
+MIT-Licensed. See LICENSE file for details.
--- badgify.js
+++ badgify.js
@@ -1,47 +1,41 @@
 #!/usr/bin/env node
-var coverageColor = function (c) {
-  var r = Math.floor(0xFF * Math.sqrt(Math.cos(c*Math.PI/200)));
-  var g = Math.floor(0xFF * Math.sqrt(Math.sin(c*Math.PI/200)));
-  var b = 0;
-  return [r,g,b].map(function (v) {
-    return (v.toString(16) + '0').slice(0, 2);
-  }).join('');
-};
+var join = require('path').join;
+var json = require(join(process.cwd(), 'package.json'));
+var hasTravis = require('fs').existsSync(join(process.cwd(), './.travis.yml'))
 
-var user = 'clux';
-var repo = 'subset';
+var repoReg = /^(\w+)\/(\w+)$/;
+if (!json.repository || !json.repository.url || !repoReg.test(json.repository.url)) {
+  throw new Error("Incompatible with npm modules without github repo urls in json");
+}
+var ghRepo = json.repository.url;
 
-// need to define or produce these somehow
-var stability = 'stable';
-var coverage = 70;
+var badges = [];
 
+if (hasTravis) {
+  var travisUrl = "travis-ci.org/" + ghRepo;
+  badges.push("[![Build Status](https://secure." + travisUrl + ".svg)](http://" + travisUrl + ")");
+}
 
-var travisUrl = "travis-ci.org/" + user + "/" + repo;
-var travis = "[![Build Status](https://secure." + travisUrl + ".svg)](http://" + travisUrl + ")";
+var davidUrl = "https://david-dm.org/" + ghRepo;
+badges.push("[![Dependency Status](" + davidUrl + ".svg)](" + davidUrl + ")");
 
-var davidUrl = "https://david-dm.org/" + user + "/" + repo;
-var david = "[![Dependency Status](" + davidUrl + ".svg)](" + davidUrl + ")";
+if (json.scripts && json.scripts.coveralls) {
+  var covUrl = "https://coveralls.io/repos/" + ghRepo;
+  var covShield = "http://img.shields.io/coveralls/" + ghRepo + ".svg";
+  badges.push("[![Coverage Status](" + covShield + "/badge.png)](" + covUrl + ")");
+}
 
-var stabilityColors = {
+var stab = json.stability || 'experimental';
+var col = {
   'deprecated': 'C62914',
   'experimental': 'DD5F0A',
   'frozen': '33C614',
   'locked': '14C6C6',
   'stable': '74C614',
   'unstable': 'E5AE13'
-};
+}[stab];
 var stabUrl = "(http://nodejs.org/api/documentation.html#documentation_stability_index)";
-var stabShield = "http://img.shields.io/badge/stability-" + stability + "-" + stabilityColors[stability] + ".svg";
-var stab = "[![" + stability + "](" + stabShield + ")](" + stabUrl + ")";
+var stabShield = "http://img.shields.io/badge/stability-" + stab + "-" + col + ".svg";
+badges.push("[![" + stab + "](" + stabShield + ")](" + stabUrl + ")");
 
-var makeCoverageShield = function (c) {
-  var covShield = "http://img.shields.io/badge/coverage-";
-  covShield += c + "%-" + coverageColor(c) + ".svg";
-  var covUrl = "https://github.com/substack/covert"; // TODO: covert output?
-  var cov = "[![coverage](" + covShield + ")](" + covUrl + ")";
-  return cov;
-}
-
-
-var res = [travis, david, stab, makeCoverageShield(coverage)];
-console.log(res.join('\n'));
+console.log(badges.join('\n'));
