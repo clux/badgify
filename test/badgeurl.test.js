a2b8c16dbafe3c4e25bc5927053b6b90997ad7c3
clux <analsandblaster@gmail.com>
2014-07-09 08:46:26 +0100

    update coveralls url and badgify

--- test/badgeurl.test.js
+++ test/badgeurl.test.js
@@ -4,7 +4,7 @@ exports.output = function (t) {
   var out = badgify(__dirname);
   t.equals(out[0], "[![Build Status](https://secure.travis-ci.org/blah/woo.svg)](http://travis-ci.org/blah/woo)", "travis");
   t.equals(out[1], "[![Dependency Status](https://david-dm.org/blah/woo.svg)](https://david-dm.org/blah/woo)", "david");
-  t.equals(out[2], "[![Coverage Status](http://img.shields.io/coveralls/blah/woo.svg)](https://coveralls.io/repos/blah/woo)", "coveralls");
+  t.equals(out[2], "[![Coverage Status](http://img.shields.io/coveralls/blah/woo.svg)](https://coveralls.io/r/blah/woo)", "coveralls");
   t.equals(out[3], "[![stable](http://img.shields.io/badge/stability-stable-74C614.svg)](http://nodejs.org/api/documentation.html#documentation_stability_index)", "stability");
   t.done();
 };
