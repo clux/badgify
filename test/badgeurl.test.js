c72d60bea987a214e892bd4425caaaf9b4b5f4b6
clux <analsandblaster@gmail.com>
2014-07-09 08:43:43 +0100

    fix tests?

--- test/badgeurl.test.js
+++ test/badgeurl.test.js
@@ -2,10 +2,9 @@ var badgify = require('../');
 
 exports.output = function (t) {
   var out = badgify(__dirname);
-  t.equals(out[0], "[![Build Status](https://secure.travis-ci.org/blah/woo.svg)](http://travis-ci.org/blah/woo)");
-  t.equals(out[1], "[![Dependency Status](https://david-dm.org/blah/woo.svg)](https://david-dm.org/blah/woo)");
-  t.equals(out[2], "[![Coverage Status](http://img.shields.io/coveralls/blah/woo.svg)](https://coveralls.io/repos/blah/woo)");
-  t.equals(out[3], "[![stable](http://img.shields.io/badge/stability-stable-74C614.svg)](http://nodejs.org/api/documentation.html#documentation_stability_index)");
-  t.equals(out[4], "");
+  t.equals(out[0], "[![Build Status](https://secure.travis-ci.org/blah/woo.svg)](http://travis-ci.org/blah/woo)", "travis");
+  t.equals(out[1], "[![Dependency Status](https://david-dm.org/blah/woo.svg)](https://david-dm.org/blah/woo)", "david");
+  t.equals(out[2], "[![Coverage Status](http://img.shields.io/coveralls/blah/woo.svg)](https://coveralls.io/repos/blah/woo)", "coveralls");
+  t.equals(out[3], "[![stable](http://img.shields.io/badge/stability-stable-74C614.svg)](http://nodejs.org/api/documentation.html#documentation_stability_index)", "stability");
   t.done();
 };
